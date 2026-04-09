ef59ba03805624db3e915d87e2324de83646d20f
DiabeticCamel <tirrellzach@gmail.com>
2026-04-09 18:36:44 -0400

    Update spotify-token.js

--- netlify/functions/spotify-token.js
+++ netlify/functions/spotify-token.js
@@ -1,1 +1,20 @@
+exports.handler = async function() {
+    const clientId = process.env.SPOTIFY_CLIENT_ID
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
 
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64')
+        },
+        body: 'grant_type=client_credentials'
+    })
+
+    const data = await response.json()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ access_token: data.access_token })
+    }
+}
