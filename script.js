1fd1fa9660ccd500b31d340214140a321a9a596c
DiabeticCamel <tirrellzach@gmail.com>
2026-04-11 17:38:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -151,14 +151,14 @@ function showAchievements() {
     `;
 
     card.innerHTML = `
-     <div class="ach-tooltip-wrap" style="position:absolute;top:7px;right:7px;">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"
-    style="width:16px;height:16px;opacity:0.45;cursor:pointer;flex-shrink:0;"
-    fill="#e3e3e3">
-    <path d="M440-280h80v-240h-80v240Zm68.5-331.5Q520-623 520-640t-11.5-28.5Q497-680 480-680t-28.5 11.5Q440-657 440-640t11.5 28.5Q463-600 480-600t28.5-11.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
-  </svg>
-  <div class="ach-tooltip">${ach.hint}</div>
-</div>
+     <div class="ach-tooltip-wrap" style="position:absolute;top:7px;right:7px;opacity:1;">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"
+          style="width:16px;height:16px;opacity:0.45;cursor:pointer;flex-shrink:0;"
+          fill="#e3e3e3">
+          <path d="M440-280h80v-240h-80v240Zm68.5-331.5Q520-623 520-640t-11.5-28.5Q497-680 480-680t-28.5 11.5Q440-657 440-640t11.5 28.5Q463-600 480-600t28.5-11.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+        </svg>
+      <div class="ach-tooltip">${ach.hint}</div>
+    </div>
       <div style="font-size:22px;">${unlocked ? ach.label.match(/\S+$/)[0] : '🔒'}</div>
       <div style="font-family:YZY;font-size:12px;color:white;">${ach.label.replace(/\S+$/, '').trim()}</div>
       <div style="font-family:SYNE;font-size:10px;color:${unlocked ? '#4daa31' : 'rgba(255,255,255,0.3)'};">
